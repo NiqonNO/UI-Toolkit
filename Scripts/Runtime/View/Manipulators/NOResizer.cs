c408d4af5725770e549812cecfb357c172e4904e
Nikodem Ocios <niqon.no@gmail.com>
2026-06-17 18:59:19 +0200

    Min size and aspect preserve

--- Scripts/Runtime/View/Manipulators/NOResizer.cs
+++ Scripts/Runtime/View/Manipulators/NOResizer.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.UIElements;
 
@@ -6,18 +7,27 @@ namespace NiqonNO.UI.View
 	public class NOResizer : PointerManipulator
 	{
 		private readonly VisualElement DragTarget;
+		private readonly Vector2 MinSize;
+		private readonly bool PreserveAspect;
 
+		public event Action<Rect> OnRectChanged;
+		
 		private bool IsActive;
 		private Vector2 Start;
 		private int PointerId;
-		
-		public NOResizer(VisualElement dragTarget)
+
+		public NOResizer(VisualElement dragTarget, bool preserveAspect = false)
 		{
 			IsActive = false;
 			PointerId = -1;
 			activators.Add(new ManipulatorActivationFilter { button = MouseButton.LeftMouse });
-			
+
+			PreserveAspect = preserveAspect;
 			DragTarget = dragTarget;
+		}
+		public NOResizer(VisualElement dragTarget, Vector2 minSize, bool preserveAspect = false) : this(dragTarget, preserveAspect)
+		{
+			MinSize = minSize;
 		}
 		
 		protected override void RegisterCallbacksOnTarget()
@@ -70,19 +80,47 @@ namespace NiqonNO.UI.View
 			target.ReleaseMouse();
 			evt.StopPropagation();
 		}
-		
+
 		private void UpdateDrag(Vector2 delta)
 		{
 			Rect container = DragTarget.hierarchy.parent.contentRect;
 			Rect position = DragTarget.layout;
-			position.width = Mathf.Clamp(position.width + delta.x, 0, container.width - position.x);
-			position.height = Mathf.Clamp(position.height + delta.y, 0, container.height - position.y);
-			
+
+
+			if (PreserveAspect)
+			{
+				float aspect = position.width / position.height;
+
+				Vector2 aspectDirection = new Vector2(1f, 1f / aspect).normalized;
+
+				float maxMagnitude = Mathf.Min(
+					(container.width - position.x - position.width) / aspectDirection.x,
+					(container.height - position.y - position.height) / aspectDirection.y);
+
+				float minMagnitude = Mathf.Max(
+					(MinSize.x - position.width) / aspectDirection.x,
+					(MinSize.y - position.height) / aspectDirection.y);
+
+				float magnitude = Mathf.Clamp(Vector2.Dot(delta, aspectDirection), minMagnitude, maxMagnitude);
+
+				delta = aspectDirection * magnitude;
+
+				position.width += delta.x;
+				position.height += delta.y;
+			}
+			else
+			{
+				position.width = Mathf.Clamp(position.width + delta.x, MinSize.x, container.width - position.x);
+				position.height = Mathf.Clamp(position.height + delta.y, MinSize.y, container.height - position.y);
+			}
+
 			DragTarget.style.top = position.y;
 			DragTarget.style.left = position.x;
-			
+
 			DragTarget.style.width = position.width;
 			DragTarget.style.height = position.height;
+
+			OnRectChanged?.Invoke(position);
 		}
 	}
 }
